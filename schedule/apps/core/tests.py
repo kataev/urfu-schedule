536dfec4afaac5e6908dc5aac6d337070883c5cd
Катаев Денис <bteamko@gmail.com>
2013-08-26 21:24:29 +0600

    Синтаксис

--- schedule/apps/core/tests.py
+++ schedule/apps/core/tests.py
@@ -1,5 +1,5 @@
-from os import environ
 from django.test import TestCase
+
 import datetime
 import requests
 import json
@@ -13,27 +13,27 @@ class GoogleCalendarApi(TestCase):
         self.assertTrue(r.ok)
 
         r = requests.get("https://www.googleapis.com/calendar/v3/users/me/calendarList",
-                   headers=authorization_header)
+                         headers=authorization_header)
         self.assertTrue(r.ok)
         return r.json()['items']
 
     def add(self, access_token, calendar_id):
         authorization_header = {"Authorization": "OAuth %s" % access_token, 'content-type': 'application/json'}
         r = requests.post("https://www.googleapis.com/calendar/v3/users/me/calendarList",
-                         headers=authorization_header, data=json.dumps({'id':calendar_id}))
+                          headers=authorization_header, data=json.dumps({'id': calendar_id}))
         self.assertTrue(r.ok)
 
 
     def delete(self, access_token, calendar_id):
         authorization_header = {"Authorization": "OAuth %s" % access_token, 'content-type': 'application/json'}
         r = requests.post("https://www.googleapis.com/calendar/v3/users/me/calendarList",
-                         headers=authorization_header, data=json.dumps({'id':calendar_id}))
+                          headers=authorization_header, data=json.dumps({'id': calendar_id}))
         self.assertTrue(r.ok)
 
     def test_add_delete_calendar(self):
         calendar_id = 'pr3dj0unj2fbtlisn4pi5boj2o@group.calendar.google.com'
-        with open('token_2', 'r') as file:
-            access_token = file.read()
+        with open('token_2', 'r') as f:
+            access_token = f.read()
         self.assertTrue(access_token)
 
         count = len(self.list(access_token))
@@ -43,32 +43,32 @@ class GoogleCalendarApi(TestCase):
 
 
     def test_add_event(self):
-        
         calendar_id = 'pr3dj0unj2fbtlisn4pi5boj2o@group.calendar.google.com'
-        with open('token_1', 'r') as file:
-            access_token = file.read()
+        with open('token_1', 'r') as f:
+            access_token = f.read()
         authorization_header = {"Authorization": "OAuth %s" % access_token, 'content-type': 'application/json'}
 
-        data = {'start':{'dateTime':datetime.datetime.utcnow().replace(hour=10).isoformat(),'timeZone':'Europe/Zurich'},
-                'end':{'dateTime':datetime.datetime.utcnow().isoformat(),'timeZone':'Europe/Zurich'},
-                'summary':'Created from api',
-                'description':'Created from api description'
-                }
-        
+        data = {
+        'start': {'dateTime': datetime.datetime.utcnow().replace(hour=10).isoformat(), 'timeZone': 'Europe/Zurich'},
+        'end': {'dateTime': datetime.datetime.utcnow().isoformat(), 'timeZone': 'Europe/Zurich'},
+        'summary': 'Created from api',
+        'description': 'Created from api description'
+        }
+
         print data
         r = requests.post("https://www.googleapis.com/calendar/v3/calendars/%s/events" % calendar_id,
-            headers=authorization_header, data=json.dumps(data))
+                          headers=authorization_header, data=json.dumps(data))
         print r.content
         self.assertTrue(r.ok)
         print r.text
 
     def test_get_event_list(self):
         calendar_id = 'pr3dj0unj2fbtlisn4pi5boj2o@group.calendar.google.com'
-        with open('token_1', 'r') as file:
-            access_token = file.read()
+        with open('token_1', 'r') as f:
+            access_token = f.read()
         authorization_header = {"Authorization": "OAuth %s" % access_token, 'content-type': 'application/json'}
 
         r = requests.get("https://www.googleapis.com/calendar/v3/calendars/%s/events" % calendar_id,
-            headers=authorization_header)
+                         headers=authorization_header)
         self.assertTrue(r.ok)
         print r.text
