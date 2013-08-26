116da2dc3601ecced4f1ae16290a6c4dd1e369b5
Kataev Denis <kataev@skydns.ru>
2013-08-26 20:09:12 +0600

    Добавление, удаление remote календаря у юзера

--- schedule/apps/core/pipeline.py
+++ schedule/apps/core/pipeline.py
@@ -1,5 +1,5 @@
 def info(backend, details, response, user=None, is_new=False, social_user=None,
          *args, **kwargs):
     if social_user:
-        with open('token', 'w+') as file:
+        with open('token_%d' % user.pk, 'w+') as file:
             file.write(social_user.tokens['access_token'])
--- schedule/apps/core/tests.py
+++ schedule/apps/core/tests.py
@@ -1,14 +1,11 @@
 from os import environ
 from django.test import TestCase
 import requests
+import json
 
 
 class GoogleCalendarApi(TestCase):
-    def test_basic(self):
-        with open('token', 'r') as file:
-            access_token = file.read()
-        self.assertTrue(access_token)
-
+    def list(self, access_token):
         authorization_header = {"Authorization": "OAuth %s" % access_token}
         r = requests.get("https://www.googleapis.com/oauth2/v2/userinfo",
                          headers=authorization_header)
@@ -17,4 +14,28 @@ class GoogleCalendarApi(TestCase):
         r = requests.get("https://www.googleapis.com/calendar/v3/users/me/calendarList",
                    headers=authorization_header)
         self.assertTrue(r.ok)
-        print r.content
+        return r.json()['items']
+
+    def add(self, access_token, calendar_id):
+        authorization_header = {"Authorization": "OAuth %s" % access_token, 'content-type': 'application/json'}
+        r = requests.post("https://www.googleapis.com/calendar/v3/users/me/calendarList",
+                         headers=authorization_header, data=json.dumps({'id':calendar_id}))
+        self.assertTrue(r.ok)
+
+
+    def delete(self, access_token, calendar_id):
+        authorization_header = {"Authorization": "OAuth %s" % access_token, 'content-type': 'application/json'}
+        r = requests.post("https://www.googleapis.com/calendar/v3/users/me/calendarList",
+                         headers=authorization_header, data=json.dumps({'id':calendar_id}))
+        self.assertTrue(r.ok)
+
+    def test_add_delete_calendar(self):
+        calendar_id = 'pr3dj0unj2fbtlisn4pi5boj2o@group.calendar.google.com'
+        with open('token_2', 'r') as file:
+            access_token = file.read()
+        self.assertTrue(access_token)
+
+        count = len(self.list(access_token))
+        self.add(access_token, calendar_id)
+        self.delete(access_token, calendar_id)
+        self.assertEqual(count, len(self.list(access_token)))
